8f0c4a71c5ce9fabb8d326a7d4c4e76201614ea5
Billy <billyfranklim@gmail.com>
2024-03-04 16:12:15 -0300

    2. 📝 chore(script): refactor get_git_diff function in script.py

--- script.py
+++ script.py
@@ -2,36 +2,46 @@ import openai
 import sys
 import os
 
-# Configure sua chave da API do OpenAI aqui
-openai.api_key = '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'
+openai.api_key = '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'
 
 def get_git_diff():
-    # Substitua 'your_diff_command' pelo comando correto para obter o diff do git
-    diff_command = 'git diff'
+    diff_command = 'git diff  --cached'
     return os.popen(diff_command).read()
 
 def generate_commit_messages(diff):
 
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo-instruct",
-        prompt=f"suggest 3 commit messages based on the following diff:\n\n{diff}\n\ncommit messages should:\n  - follow conventional commits,\n  - message format should be: <type>[scope]: <description>,\n\n\"examples:\",\n\" - 🐛 fix(authentication): add password regex pattern\",\n\" -✨ feat(storage): add new test cases\",\n",
+    response = openai.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"suggest 3 commit messages based on the following diff, if diff is empty repository e first commit :\n\n{diff}\n\ncommit messages should:\n  - follow conventional commits,\n  - message format should be: <type>[scope]: <description>,\n\n\"examples:\",\n\" - 🐛 fix(authentication): add password regex pattern\",\n\" -✨ feat(storage): add new test cases\",\n",
+            }
+        ],
+        model="gpt-3.5-turbo",
     )
-    # response = openai.ChatCompletion.create(
-    #     model="text-davinci-003",
-    #     messages=[{"role": "user", "content": f"suggest 3 commit messages based on the following diff:\n\n{diff}\n\ncommit messages should:\n  - follow conventional commits,\n  - message format should be: <type>[scope]: <description>,\n\n\"examples:\",\n\" - 🐛 fix(authentication): add password regex pattern\",\n\" -✨ feat(storage): add new test cases\",\n"}]
-    # )
 
-    return response
+    return response.choices[0].message.content
 
 def main():
     diff = get_git_diff()
-    if not diff:
-        print("No git diff found. Are you in a git repository?")
-        sys.exit(1)
 
     commit_messages = generate_commit_messages(diff)
+    commit_messages = commit_messages.split("\n")
     print("Suggested commit messages:")
-    print(commit_messages)
+
+    for message in commit_messages:
+        print(message)
+
+    selected_option = input("Choose one of the options above: ")
+    selected_option = int(selected_option)
+    if selected_option < 1 or selected_option > 3:
+        print("Invalid option")
+        sys.exit(1)
+
+    commit_message = commit_messages[selected_option - 1]
+    os.system(f'git commit -m "{commit_message}"')
+    print(f'Committed with message: "{commit_message}"')
+
 
 if __name__ == "__main__":
     main()
